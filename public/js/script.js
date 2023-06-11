c823743bf061d889f9c8e12983da5a7ebbe15d96
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2023-06-11 13:33:29 +0200

    Formulario de contacto Beta

--- public/js/script.js
+++ public/js/script.js
@@ -203,3 +203,10 @@ document.addEventListener('DOMContentLoaded', function() {
     maxZoom: 18,
   }).addTo(map);
 });
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = '';
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
+});
--- server.js
+++ server.js
@@ -13,5 +13,3 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
 });
-
-  
